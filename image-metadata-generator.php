91143ec37e9f725dfb46e176b2d42ab2d3b9db7b
JacobG321 <jacobgruber1998@gmail.com>
2024-06-14 20:53:34 -0400

    initial commit

---
+++ image-metadata-generator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Image Metadata Generator
+ * Plugin URI: https://github.com/JacobG321/image-metadata-generator
+ * Description: A plugin that generates metadata for images.
+ * Version: 0.0.1
+ * Author: Jacob Gruber
+ * Author URI: https://jgruber.dev
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
